85cb9483a43ebba7167cefba131c6a8fa49449de
Glitch (hello-webpage) <none>
2019-09-06 12:43:50 +0000

    🌑📙 Checkpoint ./users.js:452962/239 ./users.html:452962/268

--- users.js
+++ users.js
@@ -7,7 +7,7 @@ const context = () => ({
 const wait = ms => ({ then(resolve) { setTimeout(resolve, ms) } });
 
 async function loadUsers() {
-  await wait(2000);
+  await wait(3000);
   return [
     { id: 1, name: 'Wilbur' },
     { id: 2, name: 'Matthew' },
@@ -30,13 +30,20 @@ const machine = createMachine({
 const service = interpret(machine, service => {
   let state = service.machine.current;
   switch(state) {
+    case 'loading': {
+      usersNode.innerHTML = `<span class="loading">Loading</span>`;
+      break;
+    }
     case 'loaded': {
       let { users } = service.context;
       usersNode.innerHTML = `
+        <ul>
         ${users.map(user => `
           <li id="user-${user.id}">${user.name}</li>
         `).join('')}
+        </ul>
       `;
+      break;
     }
   }
 });
