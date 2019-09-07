eb0a9412bc6a34fd2edd1f2e3f5204d7e68c4d56
Glitch (hello-webpage) <none>
2019-09-07 20:09:52 +0000

    🛩🌯 Checkpoint ./preact.js:452962/182 ./login.js:452962/1322

--- login.js
+++ login.js
@@ -1,7 +1,54 @@
-import { html, Component, render } from './preact.js';
+import { h, html, Component, render } from './preact.js';
+import { createMachine, state } from './machine.js';
 
-// Create your app
-var name = 'hello';
-const app = html`Testing ${name}`;
+/*
+  context ${{ login: '', password: '' }}
+  action setLogin = assign login ${(_, ev) => ev.e.target.value}
+  action setPassword = assign password ${(_, ev) => ev.e.target.value}
+  
+  action updateSubmissionError = assign submissionError ${submissionError}
+  action clearError = assign submissionError ${() => null}
+  guard canSubmit = ${({login, password}) => login && password}
+  guard hasError = ${({ submissionError }) => !!(submissionError)}
+  initial state form {
+    login => setLogin => input
+    password => setPassword => input
+    submit => validate
+  }
+  state input {
+    => hasError => clearError => form
+    => form
+  }
+  state validate {
+    => canSubmit => complete
+    => updateSubmissionError => form
+  }
+  state complete {
+  }
 
-render(app, document.querySelector('main'));
+*/
+
+const context = () => ({ login: '', password: '' });
+
+const machine = createMachine({
+  form: state(
+  
+  ),
+  input: state(
+  
+  ),
+  validate: state(
+  
+  ),
+  complete: state()
+}, context);
+
+class App extends Component {
+  render() {
+    return html`
+      <div>Hello world</div>
+    `;
+  }
+}
+
+render(h(App), document.querySelector('main'));
--- preact.js
+++ preact.js
@@ -1,3 +1,7 @@
 export * from 'https://unpkg.com/preact@10.0.0-rc.1?module';
-export * from 'https://unpkg.com/preact@10.0.0-rc.1/hooks/dist/hooks.module.js';
-export { default as html } from 'https://unpkg.com/htm?module';
+//export * from 'https://unpkg.com/preact@10.0.0-rc.1/hooks/dist/hooks.module.js';
+
+import { h } from 'https://unpkg.com/preact@10.0.0-rc.1?module';
+import htm from 'https://unpkg.com/htm?module';
+
+export const html = htm.bind(h);
