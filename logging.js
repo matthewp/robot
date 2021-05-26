8033fcdb91eca4d1673fb132ba83ef5a9b1d5879
Daniel Große <daniel.grosse@staffbase.com>
2021-05-26 14:57:20 +0200

    fix: print objects correctly

--- logging.js
+++ logging.js
@@ -3,12 +3,14 @@ import { d } from './machine.js';
 d._onEnter = function(machine, to, state, prevState, event) {
   console.log(`Enter state ${to}`);
   console.groupCollapsed(`Details:`);
-  console.log(`Machine ${{...machine}}`);
-  console.log(`Current state ${{...state}}`);
-  console.log(`Previous state ${{...prevState}}`);
+  console.log(`Machine`, machine);
+  console.log(`Current state`, state);
+  console.log(`Previous state`, state);
 
-  if (event) {
-    console.log(`Event ${typeof event === "string" ? event : {...event}}`);
+  if (typeof event === "string") {
+    console.log(`Event ${event}`);
+  } else if (typeof event === "object") {
+    console.log(`Event`, event);
   }
 
   console.groupEnd();
