4895cf07ee0def36bbae7c1d52daa52a7320fe76
Matthew Phillips <matthew@matthewphillips.info>
2024-11-20 07:28:39 -0500

    new docs

--- docs2/astro.config.mjs
+++ docs2/astro.config.mjs
@@ -4,5 +4,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  devToolbar: {
+    enabled: false,
+  },
 });
