11f39aee30acc73a2ea4b8274f9d43fdacf3ca34
Matthew Phillips <matthew@matthewphillips.info>
2025-01-14 08:00:18 -0500

    Support typed events in hook send()

--- packages/robot-hooks/index.d.ts
+++ packages/robot-hooks/index.d.ts
@@ -1,11 +1,11 @@
 declare module 'robot-hooks' {
-  import type {Machine, SendFunction, Service} from 'robot3';
+  import type {Machine, SendFunction, Service, GetMachineTransitions} from 'robot3';
   function useMachine<M extends Machine>(
     machine: M,
     initialContext?: M['context']
   ): [
     M['state'] & {context: M['context']},
-    SendFunction,
+    SendFunction<GetMachineTransitions<M>>,
     Service<typeof machine>
   ];
 
